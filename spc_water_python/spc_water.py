af090c6a28c8e81397a2a6af631d58ec1a836c27
Christopher Lang <christopher.lang.256@gmail.com>
2024-08-23 10:41:09 +0100

    [spc_water_python] Refactor, removing periodic BCs

--- spc_water_python/spc_water.py
+++ spc_water_python/spc_water.py
@@ -1,175 +1,205 @@
+import sys, math, itertools
 import numpy as np
-import sys, math
 import progressbar
 
-def dump_timestep(timestep, atom_type, atom_position, bounding_box):
-  print("ITEM: TIMESTEP")
-  print(timestep)
-  print("ITEM: NUMBER OF ATOMS")
-  print(len(atom_type))
-  print("ITEM: BOX BOUNDS pp pp pp")
-  print(0, bounding_box[0])
-  print(0, bounding_box[1])
-  print(0, bounding_box[2])
-  print("ITEM: ATOMS id type x y z")
-  for id,(type,pos) in enumerate(zip(atom_type, atom_position)):
-    print(id, type, pos[0], pos[1], pos[2])
+# CONSTANTS
+# Units: nm, ns, u, e
 
-# Units: nm, ns, u
+# Physical Constants
+coulomb_constant = 1.38910e8 # non-trivial unit conversion
 
+# Model Constants
 OH_bond_length = 0.1
 HH_bond_length = 0.16330
 O_mass = 8
 H_mass = 1
+lennard_jones_sigma = 0.316557
+lennard_jones_epsilon = 0.650194 * 1e6 # kJ/mol * 1e6 = u(nm)^2/(ns)^2
+HW_charge = 0.410
+OW_charge = -2 * HW_charge
 
+# Simulation Configuration
 simulation_size = 2
 simulation_duration = 5e-3
+dump_rate = 10
 
-step_size = 5e-7 # realistic size
-#step_size = 1e-4
+# Numerical Constants
+step_size = 5e-7
+water_constraint_restore_displacement = 0
+water_constraint_restore_velocity = 0
+
+# Derived Constants
 num_steps = int(simulation_duration/step_size)
+# Water Constraint Matrices
+water_inverse_mass_matrix = np.linalg.inv(np.diag([O_mass]*3 + [H_mass]*6))
 
-def compute_jacobian(q, out):
-  out[0][0] = +(q[0] - q[3])
-  out[0][1] = +(q[1] - q[4])
-  out[0][2] = +(q[2] - q[5])
-  out[0][3] = -(q[0] - q[3])
-  out[0][4] = -(q[1] - q[4])
-  out[0][5] = -(q[2] - q[5])
-  out[0][6] = 0
-  out[0][7] = 0
-  out[0][8] = 0
-  out[1][0] = +(q[0] - q[6])
-  out[1][1] = +(q[1] - q[7])
-  out[1][2] = +(q[2] - q[8])
-  out[1][3] = 0
-  out[1][4] = 0
-  out[1][5] = 0
-  out[1][6] = -(q[0] - q[6])
-  out[1][7] = -(q[1] - q[7])
-  out[1][8] = -(q[2] - q[8])
-  out[2][0] = 0
-  out[2][1] = 0
-  out[2][2] = 0
-  out[2][3] = +(q[3] - q[6])
-  out[2][4] = +(q[4] - q[7])
-  out[2][5] = +(q[5] - q[8])
-  out[2][6] = -(q[3] - q[6])
-  out[2][7] = -(q[4] - q[7])
-  out[2][8] = -(q[5] - q[8])
+# SYSTEM STATE
+nmol = 0
+pos = None
+last_pos = None
+O_pos = None
+H1_pos = None
+H2_pos = None
+last_O_pos = None
+last_H1_pos = None
+last_H2_pos = None
+mass = None
+charge = None
+def create_atom_views():
+  global O_pos, H1_pos, H2_pos, last_O_pos, last_H1_pos, last_H2_pos
+  O_pos  = pos[nmol*0:nmol*1]
+  H1_pos = pos[nmol*1:nmol*2]
+  H2_pos = pos[nmol*2:nmol*3]
+  last_O_pos  = last_pos[nmol*0:nmol*1]
+  last_H1_pos = last_pos[nmol*1:nmol*2]
+  last_H2_pos = last_pos[nmol*2:nmol*3]
+def load_system_state(sys_file):
+  global pos, last_pos, mass, charge, nmol
+  simname = sys.stdin.readline().strip()
+  print(f"Reading {simname}...", file=sys.stderr)
+  natom = int(sys_file.readline())
+  assert(natom % 3 == 0)
+  nmol = natom // 3
+  pos = np.zeros((natom, 3))
+  last_pos = np.zeros((natom, 3))
+  mass = np.zeros((natom, 3))
+  mass[:nmol] = O_mass
+  mass[nmol:] = H_mass
+  charge = np.zeros((natom, 3))
+  charge[:nmol] = OW_charge
+  charge[nmol:] = HW_charge
+  create_atom_views()
+  for i in range(nmol):
+    O_mol,O_type,O_n,O_x,O_y,O_z = sys.stdin.readline().split()
+    H1_mol,H1_type,H1_n,H1_x,H1_y,H1_z = sys.stdin.readline().split()
+    H2_mol,H2_type,H2_n,H2_x,H2_y,H2_z = sys.stdin.readline().split()
+    assert(O_mol == H1_mol and O_mol == H2_mol)
+    assert(O_type == 'OW' and H1_type == 'HW1' and H2_type == 'HW2')
+    O_pos[i] = np.array([float(c) for c in (O_x, O_y, O_z)])
+    H1_pos[i] = np.array([float(c) for c in (H1_x, H1_y, H1_z)])
+    H2_pos[i] = np.array([float(c) for c in (H2_x, H2_y, H2_z)])
+  last_pos[:] = pos
+def dump_timestep(timestep):
+  global pos, O_pos, H1_pos, H2_pos
+  print("ITEM: TIMESTEP")
+  print(timestep)
+  print("ITEM: NUMBER OF ATOMS")
+  print(nmol*3)
+  print("ITEM: BOX BOUNDS pp pp pp")
+  print(0, simulation_size)
+  print(0, simulation_size)
+  print(0, simulation_size)
+  print("ITEM: ATOMS id type x y z")
+  id = 0
+  type = 1
+  for p in O_pos:
+    print(id, type, p[0], p[1], p[2])
+    id += 1
+  type = 0
+  for p in itertools.chain(H1_pos, H2_pos):
+    print(id, type, p[0], p[1], p[2])
+    id += 1
 
-mol_mass_vector = np.array([O_mass]*3+[H_mass]*6)
-mol_mass_matrix = np.diag(mol_mass_vector)
-mol_inverse_mass_matrix = np.linalg.inv(mol_mass_matrix)
+# FORCES
+# Pairwise force functions return foce on first atom in parameters.
 
-# pos, last_pos and force must be vectors of the form (Ox Oy Oz H1x H1y H1z H2x H2y H2z).
-def compute_constraint_forces(pos, last_pos, force):
-  jacobian = np.zeros((3,9))
-  compute_jacobian(pos, jacobian)
-  implied_velocity = (pos - last_pos) / step_size
-  jacobian_derivative = np.zeros((3,9))
-  compute_jacobian(implied_velocity, jacobian_derivative)
-  constraint_matrix = - np.matmul(jacobian, np.matmul(mol_inverse_mass_matrix, np.transpose(jacobian)))
-  rhs_vector = np.matmul(jacobian_derivative, implied_velocity) + np.matmul(jacobian, np.matmul(mol_inverse_mass_matrix, force))
-  constraint_parameters = np.matmul(np.linalg.inv(constraint_matrix), rhs_vector)
-  constraint_force = np.matmul(np.transpose(constraint_parameters), jacobian)
-  return constraint_force
-
-lennard_jones_sigma = 0.316557 # nm
-lennard_jones_epsilon = 0.650194 * 1e6 # kJ/mol * 1e6 = u(nm)^2/(ns)^2 (this is my base energy unit)
-coulomb_constant = 1.38910e8 # converted into my units
-
-# pass positions of oxyten, return force on O1.
-def lennard_jones_force(O1_pos, O2_pos):
+def compute_lennard_jones_force(O1_pos, O2_pos):
   r = np.linalg.norm(O2_pos - O1_pos)
   n = 24 * lennard_jones_epsilon * (math.pow(lennard_jones_sigma/r, 6) - math.pow(lennard_jones_sigma/r, 12)) / r
   return (O2_pos - O1_pos) * n / r
-def coulomb_force(P1_pos, P2_pos, P1_charge, P2_charge):
+def compute_coulomb_force(P1_pos, P2_pos, P1_charge, P2_charge):
   r = np.linalg.norm(P2_pos - P1_pos)
   n = - coulomb_constant * P1_charge * P2_charge / math.pow(r, 2)
   return (P2_pos - P1_pos) * n / r
 
-simname = sys.stdin.readline()
-print(f"Reading {simname}", file=sys.stderr)
-natoms = int(sys.stdin.readline())
-assert(natoms % 3 == 0)
-read_pos = []
-read_type = []
-read_mass = []
-for _ in range(natoms//3):
-  O_mol,O_type,O_n,O_x,O_y,O_z = sys.stdin.readline().split()
-  H1_mol,H1_type,H1_n,H1_x,H1_y,H1_z = sys.stdin.readline().split()
-  H2_mol,H2_type,H2_n,H2_x,H2_y,H2_z = sys.stdin.readline().split()
-  assert(O_mol == H1_mol and O_mol == H2_mol)
-  assert(O_type == 'OW' and H1_type == 'HW1' and H2_type == 'HW2')
-  read_pos += [float(c) for c in (O_x,O_y,O_z, H1_x,H1_y,H1_z, H2_x,H2_y,H2_z)]
-  read_type += [1,0,0]
-  read_mass += list(mol_mass_vector)
+def compute_jacobian(O_pos, H1_pos, H2_pos):
+  jacobian = np.empty((3, 9))
+  jacobian[0][0:3] = +(O_pos - H1_pos)
+  jacobian[0][3:6] = -(O_pos - H1_pos)
+  jacobian[0][6:9] = 0
+  jacobian[1][0:3] = +(O_pos - H2_pos)
+  jacobian[1][3:6] = 0
+  jacobian[1][6:9] = -(O_pos - H2_pos)
+  jacobian[2][0:3] = 0
+  jacobian[2][3:6] = +(H1_pos - H2_pos)
+  jacobian[2][6:9] = -(H1_pos - H2_pos)
+  return jacobian
+def compute_constraint_function(O_pos, H1_pos, H2_pos):
+  c = np.empty(3)
+  c[0] = 0.5 * np.linalg.norm(O_pos - H1_pos)
+  c[1] = 0.5 * np.linalg.norm(O_pos - H2_pos)
+  c[2] = 0.5 * np.linalg.norm(H1_pos - H2_pos)
+  return c
+def compute_constraint_derivative(jacobian, O_pos, H1_pos, H2_pos, last_O_pos, last_H1_pos, last_H2_pos):
+  q = np.concatenate((O_pos, H1_pos, H2_pos))
+  q_last = np.concatenate((last_O_pos, last_H1_pos, last_H2_pos))
+  vel = (q - q_last)/step_size
+  c = np.empty(3)
+  return np.matmul(jacobian, vel)
+# Returns 3 vectors corresponding to O, H1, H2.
+def compute_water_constraint_force(O_force, H1_force, H2_force, O_pos, H1_pos, H2_pos, last_O_pos, last_H1_pos, last_H2_pos):
+  O_vel = (O_pos - last_O_pos) / step_size
+  H1_vel = (H1_pos - last_H1_pos) / step_size
+  H2_vel = (H2_pos - last_H2_pos) / step_size
+  fa = np.concatenate((O_force, H1_force, H2_force))
+  q = np.concatenate((O_pos, H1_pos, H2_pos))
+  qd = np.concatenate((O_vel, H1_vel, H2_vel))
+  J = compute_jacobian(O_pos, H1_pos, H2_pos)
+  Jd = compute_jacobian(O_vel, H1_vel, H2_vel)
+  c = compute_constraint_function(O_pos, H1_pos, H2_pos)
+  cd = compute_constraint_derivative(J, O_pos, H1_pos, H2_pos, last_O_pos, last_H1_pos, last_H2_pos)
+  W = water_inverse_mass_matrix
+  ks = water_constraint_restore_displacement
+  kd = water_constraint_restore_velocity
+  JT = np.transpose(J)
+  lhs_mat = np.matmul(J, np.matmul(W, JT))
+  rhs_vec = - np.matmul(J, np.matmul(W, fa)) - np.matmul(Jd, qd) - ks * cd - kd * cd
+  constraint_parameters = np.matmul(np.linalg.inv(lhs_mat), rhs_vec)
+  constraint_force = np.matmul(np.transpose(constraint_parameters), J)
+  constraint_force.resize(3, 3)
+  return constraint_force
 
-atom_type = np.array(read_type)
-position = np.array(read_pos)
-last_position = np.copy(position)
-new_position = np.zeros(position.shape)
-mass_vector = np.array(read_mass)
+# SETUP
 
-HW_charge = 0.410 # |e| units
-OW_charge = -2 * HW_charge
+load_system_state(sys.stdin)
 
-dump_timestep(0, atom_type, np.reshape(position, (-1,3)), [simulation_size for i in range(3)])
+# TIME PROGRESSION
+
+print("Starting simulation...", file=sys.stderr)
+dump_timestep(0)
 for t in progressbar.progressbar(range(1, num_steps+1)):
-  # Force, accounting for 8 mirrors. (lol, this is only a prototype I promise!)
-  force = np.zeros(position.shape)
-  for O1 in range(0, len(position), 9):
-    for O2 in range(0, len(position), 9):
-      if O1 == O2: continue
-      for mx in [-1,0,1]:
-        for my in [-1,0,1]:
-          for mz in [-1,0,1]:
-            force[O1:O1+3] += lennard_jones_force(position[O1:O1+3], position[O2:O2+3] + np.array([mx,my,mz])*simulation_size)
-            for P1,C1 in zip(range(O1, O1+9, 3), (OW_charge, HW_charge, HW_charge)):
-              for P2,C2 in zip(range(O2, O2+9, 3), (OW_charge, HW_charge, HW_charge)):
-                P1_pos = position[P1:P1+3]
-                P2_pos = position[P2:P2+3] + np.array([mx,my,mz])*simulation_size
-                force[P1:P1+3] += coulomb_force(P1_pos, P2_pos, C1, C2)
-
-  # Periodic bc
-  for O in range(0, len(position), 9):
-    if position[O] < 0:
-      position[O] += simulation_size
-      position[O+3] += simulation_size
-      position[O+6] += simulation_size
-    if position[O-1] < 0:
-      position[O-1] += simulation_size
-      position[O-1+3] += simulation_size
-      position[O-1+6] += simulation_size
-    if position[O-2] < 0:
-      position[O-2] += simulation_size
-      position[O-2+3] += simulation_size
-      position[O-2+6] += simulation_size
-    if position[O] > simulation_size:
-      position[O] -= simulation_size
-      position[O+3] -= simulation_size
-      position[O+6] -= simulation_size
-    if position[O-1] > simulation_size:
-      position[O-1] -= simulation_size
-      position[O-1+3] -= simulation_size
-      position[O-1+6] -= simulation_size
-    if position[O-2] > simulation_size:
-      position[O-2] -= simulation_size
-      position[O-2+3] -= simulation_size
-      position[O-2+6] -= simulation_size
-
-  # constraint forces
-  for m in range(0, len(position), 9):
-    force[m:m+9] += compute_constraint_forces(position[m:m+9], last_position[m:m+9], force[m:m+9])
-  #print(np.reshape(force, (-1,3)), file=sys.stderr)
-  # TODO: light restoring force on constraints to account for numerical inaccruacy
-
-  # verlet step
-  new_position = 2 * position - last_position + step_size**2 * force / mass_vector
-  tmp = last_position
-  last_position = position
-  position = new_position
-  new_position = tmp
-  if t % 10 == 0:
-    dump_timestep(t, atom_type, np.reshape(position, (-1,3)), [simulation_size for i in range(3)])
+  # TODO: verlet lists
+  # APPLIED FORCES
+  # TODO: iterative only over verlet lists
+  force = np.zeros(pos.shape)
+  for i in range(nmol):
+    for j in range(i+1, nmol):
+      f = compute_lennard_jones_force(pos[i], pos[j])
+      force[i] += f
+      force[j] -= f
+  for i in range(nmol*3):
+    for j in range(i+1, nmol*3):
+      f = compute_coulomb_force(pos[i], pos[j], charge[i], charge[j])
+      force[i] += f
+      force[j] -= f
+  # CONSTRAINT FORCES
+  for i in range(nmol):
+    O_force = force[i]
+    H1_force = force[nmol+i]
+    H2_force = force[nmol*2+i]
+    O_fc,H1_fc,H2_fc = compute_water_constraint_force(
+        O_force, H1_force, H2_force,
+        O_pos[i], H1_pos[i], H2_pos[i],
+        last_O_pos[i], last_H1_pos[i], last_H2_pos[i],
+    )
+    force[i] += O_fc
+    force[nmol+i] += H1_fc
+    force[nmol*2+i] += H2_fc
+  # VERLET INTEGRATION
+  new_pos = 2 * pos - last_pos + step_size**2 * force / mass
+  last_pos = pos
+  pos = new_pos
+  create_atom_views()
+  # TOOD: periodic boundary conditions
+  if t % dump_rate == 0:
+    dump_timestep(t)
